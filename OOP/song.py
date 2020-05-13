7f1dbdcf6b039ed266b39110ab4dcfa3c197fcc1
Nandini Patel <47441093+nandinipatel@users.noreply.github.com>
2020-05-12 23:25:15 -0400

    Add files via upload

--- OOP/song.py
+++ OOP/song.py
@@ -20,7 +20,7 @@ class Song:
             duration(Optional[int]): Initial value for the 'duration' attribute
         """
 
-        self.title = title
+        self.name = title
         self.artist = artist
         self.duration = duration
 
@@ -54,11 +54,13 @@ class Album:
             songs(Song): A song to add
             position(Optional[int]): The song will be added to that position
         """
-
-        if position is None:
-            self.tracks.append(song)
-        else:
-            self.tracks.insert(position, song)
+        song_found = find_object(song, self.tracks)
+        if song_found is None:
+            song_found = Song(song, self.artist)
+            if position is None:
+                self.tracks.append(song_found)
+            else:
+                self.tracks.insert(position, song_found)
 
 class Artist:
     """Basic class to store artist details.
@@ -85,19 +87,61 @@ class Artist:
                 Don't add repeated albums!
         """
 
-        self.album.append(album)
+        self.albums.append(album)
+    
+    def add_song(self, name, year, title):
+        """Add a new song to the collection of albums
+
+        This method will add the song to an album in the collection.
+        
+        Args:
+            name(str): The name of the album
+            year(int): The year of the album was produced
+            title(str): The titel of the song
+        """
+        album_found = find_object(name, self.albums)
+        if album_found is None:
+            print(name + " not found")
+            album_found = Album(name, year, self)
+            self.add_album(album_found)
+        else:
+            print("Found album " + name)
+        album_found.add_song(title)
+
+def find_object(field, object_list):
+    """Check 'object_list' to see if an object with a 'name attribute equal to 'field' exists, return it if so."""
+    for item in object_list:
+        if item.name == field:
+            return item
+    return None
 
 def load_data():
-    new_artist = None
-    new_album = None
     artist_list = []
 
-    with open("albums.txt", 'r') as albums:
+    with open('albums.txt', 'r') as albums:
         for line in albums:
             #row consists of artist, album, year, song
             artist, album, year, song = tuple(line.strip('\n').split('\t'))
             year = int(year)
-            print(artist, album, year, song)
+            print("{}:{}:{}:{}".format(artist, album, year, song))
+
+            new_artist = find_object(artist, artist_list)
+            if new_artist is None:
+                new_artist = Artist(artist)
+                artist_list.append(new_artist)
+
+            new_artist.add_song(album, year, song)
+    return artist_list
+
+def create_checkfile(artist_list):
+    """Create a check file from the object data for comparision with the original file"""
+    with open("checkfile.txt", 'w') as checkfile:
+        for new_artist in artist_list:
+            for new_album in new_artist.albums:
+                for new_song in new_album.tracks:
+                    print("{0.name}\t{1.name}\t{1.year}\t{2.name}".format(new_artist, new_album, new_song), file=checkfile)
     
 if __name__ == "__main__":
-    load_data()
+    artists = load_data()
+    print("There are {} artists".format(len(artists)))
+    create_checkfile(artists)
